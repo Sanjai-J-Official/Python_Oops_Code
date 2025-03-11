41bda532681333b444412af0eb0964bc1ff52b0c
Sanjai-J-Official <thinkbigdohard@gmail.com>
2025-03-11 16:41:03 +0530

    Add Another python file

---
+++ class_object2.py
@@ -0,0 +1,15 @@
+class lap:
+    def __init__(self ,laptop,price,ram,processor):
+        self.laptop=laptop
+        self.price=price
+        self.ram=ram
+        self.processor=processor
+
+    def print_statement(self):
+        print(f'Laptop :{self.laptop}')
+        print(f'Price:{self.price}')         
+        print(f'Ram:{self.ram}')
+        print(f'Processor:{self.processor}')
+
+lap1=lap('Asus',12500,16,'AMD Ryzen')
+lap1.print_statement()
